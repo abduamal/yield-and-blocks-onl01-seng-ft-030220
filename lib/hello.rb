0bf20507d11d2237772a627695e6260c274d8a46
Amal Abdu <abduamal@ymail.com>
2020-03-08 05:30:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,11 +5,13 @@ def hello_t(array)
     while i < array.length
       yield (array[i])
       i = i + 1 
-   end
+    end
   
     array
   else
     puts "Hey! No block was given!"
+    
+  end
 end
 
 
